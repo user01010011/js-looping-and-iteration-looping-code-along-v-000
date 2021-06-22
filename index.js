49720283efac2097b396b760717d486e6ca6747a
Shasha Yan <shashayan@gmail.com>
2021-06-22 08:50:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 // Code your solutions in this file
-// function writeCards( namesArray, specialEvent ) {
-//     let thankYouCards = [ ]
-//     for (let i = 0; i < namesArray.length; i++) {
-//         console.log('Thank you, ${namesArray[i]}, for the wonderful ${specialEvent} gift!')
-//     }
-//     return thankYouCards
-// }
-
-// function countDown(10) {
-//     while (i > 0) {
-//         console.log( countdown );
-//         i -= 1;
-//     }
-//     console.log( countdown );
-// }
 
 function writeCards( namesArray, event ) {
     let thankYouCards = []
